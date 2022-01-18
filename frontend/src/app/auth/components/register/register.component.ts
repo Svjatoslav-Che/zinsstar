d5765640a92cb050ebf31ed0d29f2c170810766a
Sviatoslav <svjatoslav.cherednikov@gmail.com>
2022-01-19 00:54:31 +0200

    YA EBAL REGISTER NAHUY (est _re file - tam part of style)

--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -11,6 +11,7 @@ import {faLock as FaLock} from '@fortawesome/free-solid-svg-icons';
 export class RegisterComponent implements OnInit {
   currentForm = 0;
   faLock = FaLock;
+  currentStep: number = 1;
 
   constructor(private authService: AuthService) {
   }
@@ -24,17 +25,30 @@ export class RegisterComponent implements OnInit {
   }
 
   get personalFormGroup(): FormGroup {
+    // console.log(this.authService.formGroup1);
+    // this.currentStep = 2;
+    // console.log('Cur Step: ' + this.currentStep);
     return this.authService.formGroup1;
+
   }
 
   get contactFormGroup(): FormGroup {
+    // console.log(this.authService.formGroup2);
+    // this.currentStep = 3;
+    // console.log('Cur Step: ' + this.currentStep);
     return this.authService.formGroup2;
+
   }
 
   get additionalFormGroup(): FormGroup {
+    // console.log(this.authService.formGroup3);
+    // this.currentStep = 4;
+    // console.log('Cur Step: ' + this.currentStep);
     return this.authService.formGroup3;
+
   }
 
   ngOnInit(): void {
+    console.log('Cur Step: ' + this.currentStep);
   }
 }
